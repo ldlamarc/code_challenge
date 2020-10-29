86d47e4b66174ad3bf26173d671b8fa8809f17fb
ldlamarc <laurens.dlm@gmail.com>
2020-10-29 11:54:11 +0100

    no io yet

--- challenge.rb
+++ challenge.rb
@@ -1,15 +1,49 @@
 require 'benchmark'
 
+NUMBERS = [
+  0,
+  2,
+  12,
+  100,
+  400,
+  123456,
+  654321,
+  999999,
+  1000000000
+]
+
+CACHE = {}
+
+def convert(number)
+  return 0 if number == 0
+  return 1 if number == 1
+  return CACHE[number] if CACHE[number]
+
+  CACHE[number] = [
+    [(number/2).floor, (number/3).floor, (number/4).floor].map{ |res| convert(res)}.sum,
+    number
+  ].max
+end
+
+def numbers
+  NUMBERS
+end
+
+def output(number)
+  puts "Result: #{number}"
+end
+
+def challenge
+  numbers.map do |number|
+    output(convert(number))
+  end
+end
+
 def main
  time = Benchmark.measure do
   challenge
  end
  puts time.real
-end
-
-
-def challenge
-  1*1
 end
 
 main
